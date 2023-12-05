81741976ce8e8ac73bd6145e5507914176e27346
kalaniJ94 <kalaniJ94@gmail.com>
2023-12-04 18:15:06 -0700

    please fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kalanij94:v6BU!vnmNTKDTmF@cluster0.vd0xbcj.mongodb.net/',{
 }
 );
 
